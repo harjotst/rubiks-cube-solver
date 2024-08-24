8282e3b75ec5122c4148bb3c72df93fa282d61f6
Harjot Tathgur <tathgurharjot8@gmail.com>
2024-08-24 13:32:44 -0700

    pretty print

--- supermain.py
+++ supermain.py
@@ -10,10 +10,28 @@ class RubiksCubeSolver:
     # declare and initialize rubiks cube data structure
     def _assemble_rubiks_cube(self):
         self.rubiks_cube = []
-        for colour in [Colour.WHITE, Colour.BLUE, Colour.YELLOW, Colour.GREEN, Colour.ORANGE, Colour.RED]:
+        for colour in ['⬜️', '🟦', '🟨', '🟩', '🟧', '🟥']:
             face = [[colour, colour, colour]] * 3
             self.rubiks_cube.append(face)
 
+    def pretty_print(self):
+        rc = self.rubiks_cube
+        print('       ########')
+        print(f"       #{rc[4][0][0]}{rc[4][0][1]}{rc[4][0][2]}#")
+        print(f"       #{rc[4][1][0]}{rc[4][1][1]}{rc[4][1][2]}#")
+        print(f"       #{rc[4][2][0]}{rc[4][2][1]}{rc[4][2][2]}#")
+        print('#' * 29)
+        print(f"#{rc[0][0][0]}{rc[0][0][1]}{rc[0][0][2]}#{rc[1][0][0]}{rc[1][0][1]}{rc[1][0][2]}#{rc[2][0][0]}{rc[2][0][1]}{rc[2][0][2]}#{rc[3][0][0]}{rc[3][0][1]}{rc[3][0][2]}#")
+        print(f"#{rc[0][1][0]}{rc[0][1][1]}{rc[0][1][2]}#{rc[1][1][0]}{rc[1][1][1]}{rc[1][1][2]}#{rc[2][1][0]}{rc[2][1][1]}{rc[2][1][2]}#{rc[3][1][0]}{rc[3][1][1]}{rc[3][1][2]}#")
+        print(f"#{rc[0][2][0]}{rc[0][2][1]}{rc[0][2][2]}#{rc[1][2][0]}{rc[1][2][1]}{rc[1][2][2]}#{rc[2][2][0]}{rc[2][2][1]}{rc[2][2][2]}#{rc[3][2][0]}{rc[3][2][1]}{rc[3][2][2]}#")
+        print('#' * 29)
+        print(f"       #{rc[5][0][0]}{rc[5][0][1]}{rc[5][0][2]}#")
+        print(f"       #{rc[5][1][0]}{rc[5][1][1]}{rc[5][1][2]}#")
+        print(f"       #{rc[5][2][0]}{rc[5][2][1]}{rc[5][2][2]}#")
+        print('       ########')
+
+
+
 rubiks_cube_solver = RubiksCubeSolver()
 
-print(rubiks_cube_solver.rubiks_cube)
+rubiks_cube_solver.pretty_print()
