f2aaac330b0960a203a413b40fb89c41bee93fea
Harjot Tathgur <tathgurharjot8@gmail.com>
2024-08-25 16:03:33 -0400

    added turn layer

--- supermain.py
+++ supermain.py
@@ -1,5 +1,3 @@
-from enum import Enum
-
 class RubiksCube:
     def __init__(self):
         self._assemble_rubiks_cube()
@@ -57,6 +55,38 @@ class RubiksCube:
             for r in range(3):
                 rc[4][r][c], rc[1][r][c], rc[3][r][oc], rc[5][r][c] = rc[1][r][c], rc[5][r][c], rc[4][r][c], rc[3][r][oc]
     
+    # returns left column, top row, right column, and bottom row indices for layer
+    def _translate_layer_to_columns_and_rows(self, layer):
+        if layer == 0:
+            return 2, 2, 0, 0
+        elif layer == 1:
+            return 1, 1, 1, 1
+        else:
+            return 0, 0, 2, 2
+
+    # turn a layer clockwise or anti-clockwise
+    def _turn_layer(self, layer, clockwise=True):
+        rc = self.rubiks_cube
+        l_col, t_row, r_col, b_row = self._translate_layer_to_columns_and_rows(layer)
+        left_layer = [r[l_col] for r in rc[0]]
+        top_layer = list(rc[4][t_row])
+        right_layer = [r[r_col] for r in rc[2]]
+        bottom_layer = list(rc[5][b_row])
+        if clockwise:
+            for r in range(3):
+                rc[0][r][l_col] = bottom_layer[r]
+            rc[4][t_row] = left_layer.reverse()
+            for r in range(3):
+                rc[2][r][r_col] = top_layer[r]
+            rc[5][b_row] = right_layer.reverse()
+        else:
+            for r in range(3):
+                rc[0][r][l_col] = top_layer[2 - r]
+            rc[4][t_row] = right_layer
+            for r in range(3):
+                rc[2][r][r_col] = bottom_layer[2 - r]
+            rc[5][b_row] = left_layer
+
     # make
     def make_move(self, move):
         pass
